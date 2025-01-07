f4a271fe9913638875729a53cecdf615faa614f6
swalloich <nels.jac42@gmail.com>
2025-01-07 11:59:51 -0700

    added tracking and untracking functionality to clothing page

--- src/common/components/ClothingCard.js
+++ src/common/components/ClothingCard.js
@@ -2,19 +2,20 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import Card from './Card'
 
-function ClothingCard({id, inInventory}) {
+function ClothingCard({id, inInventory = false, setTracking}) {
   return (
     <Card>
       <img src="https://placehold.co/250" alt="" />
       <h2>{id}</h2>
-      <p>{!inInventory && 'Not '}{!inInventory ? 'i' : 'I'}n inventory</p>
+      <button onClick={() => setTracking(id, !inInventory)}>{inInventory ? 'Untrack' : 'Track'}</button>
     </Card>
   )
 }
 
 ClothingCard.propTypes = {
   id: PropTypes.string.isRequired,
-  inInventory: PropTypes.bool
+  inInventory: PropTypes.bool,
+  toggleTracking: PropTypes.func.isRequired,
 }
 
 export default ClothingCard
--- src/pages/Clothing/index.js
+++ src/pages/Clothing/index.js
@@ -1,26 +1,53 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import React from 'react'
+import React, { useCallback } from 'react'
 import LayoutBand from '../../common/components/LayoutBand'
 import useArmor from '../../common/hooks/useArmor'
 import ClothingCard from '../../common/components/ClothingCard'
 
+const gridCss = css`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+`
+
 function Clothing(props) {
-  const { allArmor } = useArmor()
+  const { allArmor, trackedArmor, trackArmor, untrackArmor } = useArmor()
+  const untrackedArmor = Object.keys(allArmor).filter((armorId) => trackedArmor?.[armorId] === undefined)
+
+  const setTracking = useCallback((armorId, isTracked) => {
+    if (isTracked) {
+      trackArmor(armorId)
+    } else if (trackedArmor?.[armorId] !== undefined) {
+      untrackArmor(armorId)
+    }
+  }, [trackedArmor, trackArmor, untrackArmor])
+
   return (
     <LayoutBand>
       <h1>Clothing</h1>
-      <div
-        css={css`
-          display: grid;
-          grid-template-columns: repeat(3, 1fr);
-          grid-gap: 20px;
-        `
-      }>
-        {Object.keys(allArmor).map((armorId) => {
-          return <ClothingCard key={armorId} id={armorId} />
-        })}
-      </div>
+      {Object.keys(trackedArmor).length > 0 && (
+        <>
+          <h2>In inventory</h2>
+          <div css={gridCss}>
+            {Object.keys(allArmor).filter((armorId) => trackedArmor?.[armorId] !== undefined).map((armorId) => {
+              const inInventory = trackedArmor?.[armorId] !== undefined
+              return <ClothingCard key={armorId} id={armorId} inInventory={inInventory} setTracking={setTracking} />
+            })}
+          </div>
+        </>
+      )}
+      {untrackedArmor.length > 0 && (
+        <>
+          <h2>Not in inventory</h2>
+          <div css={gridCss}>
+            {untrackedArmor.map((armorId) => {
+              const inInventory = trackedArmor?.[armorId] !== undefined
+              return <ClothingCard key={armorId} id={armorId} inInventory={inInventory} setTracking={setTracking} />
+            })}
+          </div>
+        </>
+      )}
     </LayoutBand>
   )
 }
