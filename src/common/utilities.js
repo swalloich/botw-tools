1000504a4293e13f580b68a1c7b32c1ae9de277a
swalloich <nels.jac42@gmail.com>
2025-02-15 16:16:18 -0700

    organize utility functions alphabetically

--- src/common/utilities.js
+++ src/common/utilities.js
@@ -1,54 +1,8 @@
-/**
- * Adds armor data to local storage.
- * @param {Object} armorData - The armor data to add to local storage.
- */
-function setArmorLocal(id, level) {
-  console.assert(typeof id === 'string', `Expected id to be a string, got ${typeof id}`)
-  console.assert(typeof level === 'number', `Expected level to be a number, got ${typeof level}`)
-  const armor = getArmorLocal()
-  armor[id] = level
-  localStorage.setItem('botw-armor', JSON.stringify(armor))
-}
-
-function setItemLocal(itemId, qty = 1) {
-  console.assert(typeof itemId === 'string', `Expected itemId to be a string, got ${typeof itemId}`)
-  console.assert(typeof qty === 'number', `Expected qty to be a number, got ${typeof qty}`)
-  const items = getAllItemsLocal()
-  const index = items.findIndex((item) => item.id === itemId)
-  if (index === -1) {
-    items.push({ id: itemId, qty })
-  } else {
-    items[index].qty += qty
+function getAllItemsLocal() {
+  if (localStorage.getItem('botw-items')) {
+    return JSON.parse(localStorage.getItem('botw-items'))
   }
-  localStorage.setItem('botw-items', JSON.stringify(items))
-}
-
-/**
- * Removes armor data from local storage.
- * @param {string} armorId - The ID of the armor to remove from local storage.
- * @returns {boolean} - True if the armor was removed, false if it was not found.
- */
-function removeArmorLocal(armorId) {
-  console.assert(typeof armorId === 'string', `Expected armorId to be a string, got ${typeof armorId}`)
-  const armor = getArmorLocal()
-  if (armor[armorId] === undefined) {
-    return false
-  }
-  delete armor[armorId]
-  localStorage.setItem('botw-armor', JSON.stringify(armor))
-  return true
-}
-
-function removeItemLocal(itemId) {
-  console.assert(typeof itemId === 'string', `Expected itemId to be a string, got ${typeof itemId}`)
-  const items = getAllItemsLocal()
-  const index = items.findIndex((item) => item.id === itemId)
-  if (index === -1) {
-    return false
-  }
-  items.splice(index, 1)
-  localStorage.setItem('botw-items', JSON.stringify(items))
-  return true
+  return []
 }
 
 /**
@@ -79,11 +33,57 @@ function getItemLocal(itemId) {
   return items.find((item) => item.id === itemId)
 }
 
-function getAllItemsLocal() {
-  if (localStorage.getItem('botw-items')) {
-    return JSON.parse(localStorage.getItem('botw-items'))
-  }
-  return []
+/**
+ * Adds armor data to local storage.
+ * @param {Object} armorData - The armor data to add to local storage.
+ */
+function setArmorLocal(id, level) {
+  console.assert(typeof id === 'string', `Expected id to be a string, got ${typeof id}`)
+  console.assert(typeof level === 'number', `Expected level to be a number, got ${typeof level}`)
+  const armor = getArmorLocal()
+  armor[id] = level
+  localStorage.setItem('botw-armor', JSON.stringify(armor))
 }
 
-export { getArmorLocal, getItemLocal, getAllItemsLocal, removeArmorLocal, removeItemLocal, setArmorLocal, setItemLocal }
+/**
+ * Removes armor data from local storage.
+ * @param {string} armorId - The ID of the armor to remove from local storage.
+ * @returns {boolean} - True if the armor was removed, false if it was not found.
+ */
+function removeArmorLocal(armorId) {
+  console.assert(typeof armorId === 'string', `Expected armorId to be a string, got ${typeof armorId}`)
+  const armor = getArmorLocal()
+  if (armor[armorId] === undefined) {
+    return false
+  }
+  delete armor[armorId]
+  localStorage.setItem('botw-armor', JSON.stringify(armor))
+  return true
+}
+
+function removeItemLocal(itemId) {
+  console.assert(typeof itemId === 'string', `Expected itemId to be a string, got ${typeof itemId}`)
+  const items = getAllItemsLocal()
+  const index = items.findIndex((item) => item.id === itemId)
+  if (index === -1) {
+    return false
+  }
+  items.splice(index, 1)
+  localStorage.setItem('botw-items', JSON.stringify(items))
+  return true
+}
+
+function setItemLocal(itemId, qty = 1) {
+  console.assert(typeof itemId === 'string', `Expected itemId to be a string, got ${typeof itemId}`)
+  console.assert(typeof qty === 'number', `Expected qty to be a number, got ${typeof qty}`)
+  const items = getAllItemsLocal()
+  const index = items.findIndex((item) => item.id === itemId)
+  if (index === -1) {
+    items.push({ id: itemId, qty })
+  } else {
+    items[index].qty += qty
+  }
+  localStorage.setItem('botw-items', JSON.stringify(items))
+}
+
+export { getAllItemsLocal, getArmorLocal, getItemLocal, removeArmorLocal, removeItemLocal, setArmorLocal, setItemLocal }
