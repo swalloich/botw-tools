3c009620dbd364bc696e5df152781eb0a97eb116
swalloich <nels.jac42@gmail.com>
2025-02-17 21:21:05 -0700

    added grid to Clothing page

--- src/pages/Clothing/index.js
+++ src/pages/Clothing/index.js
@@ -1,15 +1,6 @@
 import React from 'react'
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
-import LayoutBand from '../../common/components/LayoutBand'
 import useArmor from '../../common/hooks/useArmor'
-import ClothingCard from '../../common/components/ClothingCard'
-
-const gridCss = css`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 20px;
-`
+import { ClothingCard, Grid, LayoutBand } from '../../common/components'
 
 function Clothing(props) {
   const { armor } = useArmor()
@@ -17,11 +8,11 @@ function Clothing(props) {
   return (
     <LayoutBand>
       <h1>Clothing</h1>
-      <div css={gridCss}>
+      <Grid>
         {armor.map((armor) => (
           <ClothingCard key={armor.slug} data={armor} />
         ))}
-      </div>
+      </Grid>
     </LayoutBand>
   )
 }
