1ce05336009fed217bfec83aca2647b374a67f96
swalloich <nels.jac42@gmail.com>
2025-03-01 23:04:18 -0700

    removed unused vars and imports

--- src/pages/Items/index.js
+++ src/pages/Items/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { LayoutBand } from '../../common/components'
-import useItems from '../../common/hooks/useItems'
 import ItemDataGrid from './ItemDataGrid'
 
-function Items(props) {
-  const { data } = useItems()
-
+function Items() {
   return (
     <LayoutBand>
       <h1>Items</h1>
