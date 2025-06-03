2f7c1fc2ef51745b190fe0f7ca192bc3035e6c97
Jacob Nelson <nels.jac42@gmail.com>
2025-06-03 12:30:20 -0600

    Fixed issues with mobile menu (#10)

--- src/common/components/PageWrapper/Header.js
+++ src/common/components/PageWrapper/Header.js
@@ -1,16 +1,35 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import LayoutBand from '../LayoutBand'
 import NavBar from '../NavBar'
 import HeaderFooterWrapper from './HeaderFooterWrapper'
+import useDeviceWidth from '../../hooks/useDeviceWidth'
 
-const layoutBandCss = {
-  alignItems: 'center',
-  justifyContent: 'space-between'
-}
+const headerCss = css`
+  &[data-is-mobile] {
+    overflow-x: clip;
+    position: sticky;
+    top: -1px;
+  }
+`
+
+const layoutBandCss = css`
+  align-items: center;
+  gap: 1rem;
+  position: relative;
+
+  &[data-is-mobile] {
+    justify-content: space-between;
+  }
+`
+
 
 export default function Header({ children, links, ...props }) {
+  const { atWidth } = useDeviceWidth()
+  const dataIsMobile = atWidth({ default: '', sm: undefined })
   return (
-    <HeaderFooterWrapper Element='header' {...props}>
-      <LayoutBand css={layoutBandCss} direction='row'>
+    <HeaderFooterWrapper css={headerCss} data-is-mobile={dataIsMobile} Element='header' {...props}>
+      <LayoutBand css={layoutBandCss} data-is-mobile={dataIsMobile} direction='row'>
         <img src="https://placehold.co/50x50" alt="" width={50} height={50} />
         <NavBar links={links} />
       </LayoutBand>
